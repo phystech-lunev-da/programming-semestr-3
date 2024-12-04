ab73d3586e657e6f3ab1ea95948e8a32da2394e5
Lunev Dmitriy <lunev.da@phystech.edu>
2024-12-04 11:53:16 +0300

    Add Node class

--- skiplist/include/skiplist.hpp
+++ skiplist/include/skiplist.hpp
@@ -1,8 +1,19 @@
 
 #include <iterator>
+#include <forward_list>
+
+template<typename T>
+class Node {
+public:
+    T get() const;
+    void set(T);
+private:
+    T data;
+};
 
 template<typename T> 
 class SkipList {
+public:
     SkipList();
 
     SkipList(const SkipList&);
@@ -11,7 +22,17 @@ class SkipList {
     bool empty();
     unsigned size();
     void insert(T);
+private:
+    static const unsigned lines_count = 4;
 
-
-
+    
 };
+
+template<typename T>
+SkipList<T>::SkipList() : head(nullptr) {
+    lines[0] = nullptr;
+    lines[lines_count - 1] = head;
+    for (unsigned i = 0; i < lines_count; i++) {
+        head
+    }
+}
